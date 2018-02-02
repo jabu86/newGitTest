e7e44727a890497d03e5fb94515dc81a949e7e43
logikaltimesheet <jabumadela86@gmail.com>
2018-02-02 10:53:50 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Admin Area</title>
+
+    <title>calendar</title>
+	<script type="text/javascript" src="bootstrap.js"></script>
+	<link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="style.css">
+	
+  </head>
+
+  <body class="bg-light">
+
+    
+	<footer class="container">
+	
+		<?php 
+			echo date('Y m ')."<p>Jabu Madela &copy;</p>";
+		?>
+	</footer>
+  </body>
+</html>
